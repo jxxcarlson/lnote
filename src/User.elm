3b127503776711cf3f53909819205982a4457f69
James Carlson <jxxcarlson@mac.com>
2019-08-10 20:07:31 +0200

    Experiment: remove old encryption option

--- src/User.elm
+++ src/User.elm
@@ -72,7 +72,11 @@ encrypt str =
 
 validatePassword : String -> String -> Bool
 validatePassword password encryptedPassword =
-    encrypt password == encryptedPassword || encrypt1 password == encryptedPassword
+    encrypt password == encryptedPassword
+
+
+
+-- || encrypt1 password == encryptedPassword
 
 
 validateUser : PasswordDict -> Username -> String -> Bool
